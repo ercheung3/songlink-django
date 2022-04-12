77449537a22942f66b059f9a244131cfdb51c51e
ercheung3 <ercheung3@gmail.com>
2022-04-11 22:02:46 -0700

    Added django models.

---
+++ lyrics_api/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Lyrics
+admin.site.register(Lyrics)
---
+++ lyrics_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LyricsApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'lyrics_api'
